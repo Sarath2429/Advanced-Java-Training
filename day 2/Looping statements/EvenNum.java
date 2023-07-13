0ac1a19c4107f600f8dace84171ed95711331cad
Sarath2429 <139304167+Sarath2429@users.noreply.github.com>
2023-07-12 21:04:23 -0700

    Add files via upload

---
+++ day 2/Looping statements/EvenNum.java
@@ -0,0 +1,14 @@
+ class EvenNum 
+{  
+public static void main(String args[])   
+{  
+int number=100;  
+for (int i=1; i<=number; i++)   
+{ 
+if (i%2==0)   
+{  
+System.out.print(i + " ");  
+}  
+}  
+}  
+}  
